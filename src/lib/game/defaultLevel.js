4328a4d846c8df06206dafbbd01ee8927b11d633
lloyet <contact@lloyet.dev>
2026-05-05 13:27:27 +0200

    feat(example): add spike and player

--- src/lib/game/defaultLevel.js
+++ src/lib/game/defaultLevel.js
@@ -1,7 +1,5 @@
 export const defaultLevel = `
-<!-- ═══════════════════════════════
-     SOL DU NIVEAU  (ligne 15 = bas)
-     ═══════════════════════════════ -->
+<!-- ═════ SOL DU NIVEAU  (ligne 15 = bas) ══════ -->
 <h1 data-x="1"  data-y="15"></h1>
 <h1 data-x="2"  data-y="15"></h1>
 <h1 data-x="3"  data-y="15"></h1>
@@ -12,7 +10,9 @@ export const defaultLevel = `
 <h1 data-x="8"  data-y="15"></h1>
 <h1 data-x="9"  data-y="15"></h1>
 <h1 data-x="10" data-y="15"></h1>
+
 <!-- TROU en colonne 11 : Mario peut tomber ! -->
+
 <h1 data-x="12" data-y="15"></h1>
 <h1 data-x="13" data-y="15"></h1>
 <h1 data-x="14" data-y="15"></h1>
@@ -28,37 +28,27 @@ export const defaultLevel = `
 <h1 data-x="24" data-y="15"></h1>
 <h1 data-x="25" data-y="15"></h1>
 
-<!-- ═══════════════════
-     PLATEFORMES BRIQUES
-     ═══════════════════ -->
+<!-- ═══════ PLATEFORMES BRIQUES ══════ -->
 <h2 data-x="4"  data-y="12"></h2>
 <h2 data-x="5"  data-y="12"></h2>
 <h2 data-x="6"  data-y="12"></h2>
 <h2 data-x="7"  data-y="12"></h2>
 
-<!-- ══════════════════
-     BLOCS QUESTION (?)
-     ══════════════════ -->
+<!-- ══════ BLOCS QUESTION (?) ════════ -->
 <h3 data-x="10" data-y="11"></h3>
 <h3 data-x="14" data-y="10"></h3>
 <h3 data-x="20" data-y="10"></h3>
 
-<!-- ══════════
-     TUYAUX
-     ══════════ -->
+<!-- ══════ TUYAUX ══════════ -->
 <a id="tuyau-1" data-x="10" data-y="14" href="#tuyau-2"></a>
 <a id="tuyau-2" data-x="13" data-y="14" href="#tuyau-1"></a>
 
-<!-- ═════════════════════
-     NUAGES (plateformes)
-     ═════════════════════ -->
+<!-- ══════════ NUAGES (plateformes) ═════════════ -->
 <h5 data-x="16" data-y="10"></h5>
 <h5 data-x="17" data-y="10"></h5>
 <h5 data-x="18" data-y="10"></h5>
 
-<!-- ══════
-     PIÈCES
-     ══════ -->
+<!-- ══════ PIÈCES ══════ -->
 <p data-x="4"  data-y="11"></p>
 <p data-x="5"  data-y="11"></p>
 <p data-x="6"  data-y="11"></p>
@@ -69,13 +59,15 @@ export const defaultLevel = `
 <p data-x="18" data-y="9"></p>
 <p data-x="20" data-y="9"></p>
 
-<!-- ════════════════════════════════════
-     ENNEMIS  (saute dessus pour vaincre)
-     ════════════════════════════════════ -->
+<!-- ═══════ PIÈGES ═══════ -->
+<hr data-x="20" data-y="8" />
+
+<!-- ═══════ JOUEUR (mario) ═══════ -->
+<input name="mario" class="red" data-x="0" data-y="14" />
+
+<!-- ═══════ ENNEMIS  (saute dessus pour vaincre ═════════════ -->
 <input name="goomba" data-x="22" data-y="14" />
 
-<!-- ═══════════════════════
-     DRAPEAU (objectif final)
-     ═══════════════════════ -->
+<!-- ═════ DRAPEAU (objectif final) ═════ -->
 <h6 data-x="25" data-y="11"></h6>
 `.trim();
